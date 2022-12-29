327caeaecd85cf0896e4032955a6c48a4a17cea8
Ifta Rodrigues <ifta.moraes@maxiconsystems.com.br>
2022-12-29 13:37:53 -0300

    add: colocando os mocks e criando o service para simular acesso a esses dados

---
+++ src/mocks/topo.js
@@ -0,0 +1,6 @@
+const topo = {
+    boasVindas: "Olá Ifta",
+    legenda: "Encontre os melhores produtores",
+};
+
+export default topo;
---
+++ src/services/carregaDados.js
@@ -0,0 +1,10 @@
+import produtores from '../mocks/Produtores';
+import topo from '../mocks/topo';
+
+export function carregaProdutores() {
+  return produtores;
+}
+
+export function carregaTopo() {
+  return topo;
+}

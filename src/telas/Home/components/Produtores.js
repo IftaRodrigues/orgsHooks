4f6cc77aacf9554651ea3f5c69fb73ee44a6a83f
Ifta Rodrigues <ifta.moraes@maxiconsystems.com.br>
2023-01-05 11:41:45 -0300

    add: usando o useEffect e useState

---
+++ src/telas/Home/components/Produtores.js
@@ -0,0 +1,42 @@
+import React, {useEffect, useState} from 'react';
+import {FlatList, StyleSheet, Text} from 'react-native';
+import {carregaProdutores} from '../../../services/carregaDados';
+
+function Produtores({topo: Topo}) {
+  const [titulo, setTitulo] = useState('');
+  const [lista, setLista] = useState([]);
+
+  useEffect(() => {
+    const retorno = carregaProdutores();
+    setTitulo(retorno.titulo);
+    setLista(retorno.lista);
+  }, []);
+
+  const TopoLista = () => (
+    <>
+      <Topo />
+      <Text style={styles.titulo}>{titulo}</Text>
+    </>
+  );
+  return (
+    <FlatList
+      data={lista}
+      renderItem={({item: {nome}}) => <Text>{nome}</Text>}
+      keyExtractor={({nome}) => nome}
+      ListHeaderComponent={TopoLista}
+    />
+  );
+}
+
+const styles = StyleSheet.create({
+  titulo: {
+    fontSize: 20,
+    lineHeight: 32,
+    marginHorizontal: 16,
+    marginTop: 16,
+    fontWeight: 'bold',
+    color: '#464646',
+  },
+});
+
+export default Produtores;

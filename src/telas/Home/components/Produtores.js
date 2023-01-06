27e39d167943ec7f0f4d1b35a3b3ebc1fd846bdd
Ifta Rodrigues <ifta.moraes@maxiconsystems.com.br>
2023-01-05 21:24:14 -0300

    add: criando um hook customizado

--- src/telas/Home/components/Produtores.js
+++ src/telas/Home/components/Produtores.js
@@ -1,17 +1,10 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 import {FlatList, StyleSheet, Text} from 'react-native';
-import {carregaProdutores} from '../../../services/carregaDados';
 import Produtor from './Produtor';
+import useProdutores from '../../../hooks/useProdutores';
 
 function Produtores({topo: Topo}) {
-  const [titulo, setTitulo] = useState('');
-  const [lista, setLista] = useState([]);
-
-  useEffect(() => {
-    const retorno = carregaProdutores();
-    setTitulo(retorno.titulo);
-    setLista(retorno.lista);
-  }, []);
+  const [titulo, lista] = useProdutores();
 
   const TopoLista = () => (
     <>
